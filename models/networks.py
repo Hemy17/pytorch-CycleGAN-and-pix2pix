03b0eabbd322b4d3ec5b76f87bc20ceb8daf9d37
Hemy17 <1404263636@qq.com>
2025-03-19 10:23:11 -0700

    fixed ctrl_dim

--- models/networks.py
+++ models/networks.py
@@ -468,7 +468,7 @@ class UnetGenerator(nn.Module):
             )
 
         # construct unet structure
-        unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)  # add the innermost layer
+        unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=1024, submodule=None, norm_layer=norm_layer, innermost=True)  # add the innermost layer
         for i in range(num_downs - 6):      # 5    # add intermediate layers with ngf * 8 filters
             unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer, use_dropout=use_dropout)
         # gradually reduce the number of filters from ngf * 8 to ngf
