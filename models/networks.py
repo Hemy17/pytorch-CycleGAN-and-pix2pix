9dfe02eac82856c6d8a597a1db726d8d70bca65e
Hemy17 <1404263636@qq.com>
2025-03-19 15:54:56 -0700

    fixed ctrl_dim

--- models/networks.py
+++ models/networks.py
@@ -644,7 +644,7 @@ class UnetSkipConnectionBlock(nn.Module):
         skip_x = x 
         for layer in self.model:
             if isinstance(layer, UnetSkipConnectionBlock):  
-                x = layer(x, control_embedding  if self.innermost else None)
+                x = layer(x, control_embedding)
             else:
                 x = layer(x)
         #return self.model(x)
