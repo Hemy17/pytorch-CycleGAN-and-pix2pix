1950d857731e2f30a0a84eda7dc121cb572165ec
Hemy17 <1404263636@qq.com>
2025-03-19 09:35:42 -0700

    fixed ctrl_dim

--- models/networks.py
+++ models/networks.py
@@ -485,8 +485,9 @@ class UnetGenerator(nn.Module):
         #control_embedding = control_embedding.view(control_embedding.shape[0], -1, 2, 2) 
         #control_embedding = control_embedding.view(control_embedding.shape[0], 256, 2, 2) 
         control_embedding = control_embedding.view(control_embedding.shape[0], 1024, 1, 1)  
-        control_embedding = F.interpolate(control_embedding, size=(4, 4), mode='bilinear', align_corners=True)
-        control_embedding = control_embedding.view(control_embedding.shape[0], 256, 4, 4)
+        control_embedding = control_embedding.expand(-1, -1, 4, 4)
+        #control_embedding = F.interpolate(control_embedding, size=(4, 4), mode='bilinear', #align_corners=True)
+        #control_embedding = control_embedding.view(control_embedding.shape[0], 256, 4, 4)
         print(f"[UnetGenerator] control_embedding (reshaped).shape: {control_embedding.shape}")
 
         return self.model(input, control_embedding)  # output control_embedding
