eebc7cac1df832196009a48f0cbc018d25626988
Hemy17 <1404263636@qq.com>
2025-03-19 17:38:49 -0700

    Film

--- models/networks.py
+++ models/networks.py
@@ -531,6 +531,9 @@ class UnetSkipConnectionBlock(nn.Module):
         if input_nc is None:
             input_nc = outer_nc
         
+        
+        self.film_gamma = nn.Linear(inner_nc, outer_nc)
+        self.film_beta = nn.Linear(inner_nc, outer_nc)
         #print(f"\n[UnetSkipConnectionBlock Initializing]")
         #print(f"  |- layer type: {'outermost' if outermost else 'innermost' if innermost else 'medium'}")
         #print(f"  |- outer_nc: {outer_nc}")
@@ -588,8 +591,7 @@ class UnetSkipConnectionBlock(nn.Module):
 
         # self.model = nn.ModuleList(model)
 
-        self.film_gamma = nn.Linear(inner_nc, outer_nc)
-        self.film_beta = nn.Linear(inner_nc, outer_nc)
+
 
     '''
     def forward(self, x, control_embedding=None):
